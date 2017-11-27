baf1b7563aa141f79ae3f4cc72d790647051f58b
Hannah Dadd <hannahbdadd@googlemail.com>
2017-11-27 20:58:34 +0000

    FIN- Create dataset from bing of famous people

--- src/main/java/uk/ac/soton/ecs/hbd1g15/ch6/App.java
+++ src/main/java/uk/ac/soton/ecs/hbd1g15/ch6/App.java
@@ -2,13 +2,16 @@ package uk.ac.soton.ecs.hbd1g15.ch6;
 
 import java.util.Map.Entry;
 
+import org.openimaj.data.dataset.MapBackedDataset;
 import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
 import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.dataset.BingImageDataset;
 import org.openimaj.image.dataset.FlickrImageDataset;
 import org.openimaj.util.api.auth.DefaultTokenFactory;
+import org.openimaj.util.api.auth.common.BingAPIToken;
 import org.openimaj.util.api.auth.common.FlickrAPIToken;
 
 /**
@@ -18,12 +21,7 @@ import org.openimaj.util.api.auth.common.FlickrAPIToken;
 public class App {
     public static void main( String[] args ) {
     	try {
-			VFSListDataset<FImage> images = 
-					new VFSListDataset<FImage>("/data", ImageUtilities.FIMAGE_READER);
-			
-			// Display a random image
-			DisplayUtilities.display(images.getRandomInstance(), "A random image from the dataset");
-			
+    		// Excercies 2- other sources supported for building datasets: Zip files, local disk or HTTP server
 			// Create image data set from images held in a zip file
 			VFSListDataset<FImage> faces = 
 					new VFSListDataset<FImage>("zip:http://datasets.openimaj.org/att_faces.zip", ImageUtilities.FIMAGE_READER);
@@ -35,8 +33,22 @@ public class App {
 			
 			// Have to loop through keys, generated from the directory names of images to display them
 			for (final Entry<String, VFSListDataset<FImage>> entry : groupedFaces.entrySet()) {
-				DisplayUtilities.display(entry.getKey(), entry.getValue());
+				
+				// Exercise 1- Display a random image of each face from the dataset
+				DisplayUtilities.display(entry.getValue().getRandomInstance());
 			}
+			
+			// Exercise 3- Bing image dataset
+			BingAPIToken bingToken = DefaultTokenFactory.get(BingAPIToken.class);
+			BingImageDataset<FImage> famousOne =
+					BingImageDataset.create(ImageUtilities.FIMAGE_READER, bingToken, "Prince Harry", 10);
+			BingImageDataset<FImage> famousTwo =
+					BingImageDataset.create(ImageUtilities.FIMAGE_READER, bingToken, "Jane Austen", 10);
+			BingImageDataset<FImage> famousThree =
+					BingImageDataset.create(ImageUtilities.FIMAGE_READER, bingToken, "JK Rolling", 10);
+			// Exercise 4- Make data set of famous people
+			MapBackedDataset.of(famousOne, famousTwo, famousThree);
+			DisplayUtilities.display("Dogs", famousThree);
 			
 			// Flickr finds tagged images, can dynamically construct a dataset from a Flickr search
 			FlickrAPIToken flickrToken = DefaultTokenFactory.get(FlickrAPIToken.class);
